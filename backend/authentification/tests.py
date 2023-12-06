a753719b7840be5a784ee0f1e84d682a0b3cc153
mydream1028 <gooddream1028@gmail.com>
2023-12-06 21:40:08 +0900

    feat: test authentification

--- backend/authentification/tests.py
+++ backend/authentification/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your tests here.
+
+class GetUserViewTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.client = APIClient()
+        self.token = RefreshToken.for_user(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {str(self.token.access_token)}')
+
+    def test_home_view(self):
+        url = reverse('getUser')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['username'], 'testuser')
+        self.assertEqual(response.data['email'], '')
+        self.assertEqual(response.data['id'], str(self.user.id))
+
+
+class LogoutViewTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.client = APIClient()
+        self.token = RefreshToken.for_user(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {str(self.token.access_token)}')
+
+    def test_logout_view(self):
+        url = reverse('logout')
+        data = {'refresh_token': str(self.token)}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_205_RESET_CONTENT)
+
+
+class RegisterViewTest(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_register_view(self):
+        url = reverse('register')
+        data = {'username': 'testuser', 'password': 'testpass', 'email': 'testuser@example.com'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
