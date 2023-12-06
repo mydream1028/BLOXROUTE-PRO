bf76ba0fc365909dae0fe727a94f46cdc4960080
mydream1028 <gooddream1028@gmail.com>
2023-12-06 21:02:40 +0900

    feat: configure authentification/views.py(GetUser, Logout, Register)

--- backend/authentification/views.py
+++ backend/authentification/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 
-# Create your views here.
+from django.contrib.auth.models import User
+  
+class GetUserView(APIView):
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request):
+        content = {'username': str(request.user.username), 'email': str(request.user.email), 'id': str(request.user.id)}
+        return Response(content)
+
+
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+
+class RegisterView(APIView):
+    def post(self, request):
+        # Get the user data from the request
+        username = request.data.get('username')
+        password = request.data.get('password')
+        email = request.data.get('email')
+
+        if not username or not password or not email:
+            return Response({'error': 'Username, password, and email are required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            validate_email(email)
+        except ValidationError:
+            return Response({'error': 'Invalid email address.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Check if the user already exists
+        if User.objects.filter(username=username).exists():
+            return Response({'error': 'Username already exists.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Create the user
+        user = User.objects.create_user(username=username, password=password, email=email)
+        user.save()
+
+        # Return a success response
+        return Response({'message': 'User created successfully.'}, status=status.HTTP_201_CREATED)
