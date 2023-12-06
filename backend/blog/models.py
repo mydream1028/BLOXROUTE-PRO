83ae070b695bea96dcf56923d85bdb6c9505c96c
mydream1028 <gooddream1028@gmail.com>
2023-12-06 21:56:47 +0900

    feat: configure blog model

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Keyword(models.Model):
+    word = models.CharField("word", max_length=100, primary_key=True)
+
+    def __str__(self):
+        return self.word
+
+
+class Blog(models.Model):
+    postId = models.BigAutoField(primary_key=True)
+    authorId = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField("title", max_length=100)
+    content = models.TextField("content", max_length=1000)
+    keyword = models.ManyToManyField(Keyword)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
